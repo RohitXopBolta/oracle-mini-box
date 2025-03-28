df2cb0ea29313d69b35eefd4d097ea648e4b72f3
Helladox <koreanserverff444@gmail.com>
2025-03-28 17:20:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello from Tech VJ'
 
-
 if __name__ == "__main__":
-    app.run()
-  
+    # Get the port from the environment variable, default to 5000 if not set
+    port = int(os.environ.get("PORT", 5000))
+    
+    # Run the app on the dynamic port
+    app.run(host="0.0.0.0", port=port)
