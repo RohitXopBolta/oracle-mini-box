c4a42d396ba3772a8591b06f7e095c3413c7e1f2
helladox <koreanserverff444@gmail.com>
2025-03-28 17:05:43 +0530

    Initial commit

---
+++ config.py
@@ -0,0 +1,29 @@
+import os
+
+class Config(object):
+    # Telegram Bot ka token
+    BOT_TOKEN = "7519785012:AAGYVkj26sgetUlAiyA5c3F37XyRIxyErK0"
+    # Telegram API ki ID
+    API_ID = 22581733
+    API_HASH = "1db7bdcf908100cc641c6a5276765c3d"
+    ADMIN_ID = [6530997270]
+    # MongoDB database ka URL
+    DB_URL = "mongodb+srv://neoayush444:3kuwGf9jKicbDxvT@cluster0.f9vq7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    # Database ka naam
+    DB_NAME = "MY_BOT_DB"
+    # Text log channel ki ID
+    TXT_LOG = -1002487777733
+    # Authentication log channel ki ID
+    AUTH_LOG = -1002487777733
+    # Hit log channel ki ID
+    HIT_LOG = -1002487777733
+    # DRM dump channel ki ID
+    DRM_DUMP = -1002487777733
+    # Main channel ki ID
+    CHANNEL = -1002487777733
+    # Channel ka link
+    CH_URL = "https://t.me/+qEEho66dZJ1kODRi"
+    # Bot ke owner ka Telegram link
+    OWNER = "https://t.me/slayerxcorp"
+    # Thumbnail image ka URL
+    THUMB_URL = "https://telegra.ph/file/example-thumb-image.jpg" #Replace by with your Thumb URL
