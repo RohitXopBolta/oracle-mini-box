4168c11bd12b53b796c6b5f935c6e5b96eb46abf
Helladox <koreanserverff444@gmail.com>
2025-03-30 19:06:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
-import os
-
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello from Tech VJ'
 
-if __name__ == "__main__"
+
+if __name__ == "__main__":
+    app.run()
+  
